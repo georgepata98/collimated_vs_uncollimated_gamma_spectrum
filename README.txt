f31bc3e9698347f81216a7156a6e08213d05f4f2
George <122010570+georgepata98@users.noreply.github.com>
2026-08-26 13:05:56 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+Se testeaza cum se modifica spectrul fotonilor care ajung la un detector virtual (un anumit punct in spatiu) in functie de daca sursa este sau nu colimata.
+
+- In "main.cc" se modifica nr. de evenimente per run.
+- In "construction.hh" se modifica collimatedGeom in true sau false dupa caz (cu sau fara colimator).
+- In "run.cc" se modifica nr. bini, E_min si E_max din spectru.
+- In "gps.mac" se modifica energia gammelor.
---
+++ main.cc
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <climits>
+
+#include "G4AnalysisManager.hh"
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "G4VisManager.hh"
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+#include "G4ScoringManager.hh"
+
+#include "Randomize.hh"
+
+#include "construction.hh"
+#include "physics.hh"
+#include "action.hh"
+
+int main(int argc, char** argv)
+{
+    G4int nOfRuns = 1;  // de selectat nr. de Run-uri
+    G4bool visualization = false;  // true = cu vizualizare si false = fara vizualizare
+
+    G4int visEvents = 15;  // nr. events cu GUI
+    // G4int eventsPerRun = 2000000000;  // nr. ~maxim care depinde de int
+    G4int eventsPerRun = 5000000;
+
+    /* ATENTIE: Loop-ul cu Run-uri se executa numai cand visualization = false */
+
+
+    //Initializarea simularii
+    G4UImanager *UImanager = G4UImanager::GetUIpointer();
+    G4ScoringManager::GetScoringManager();
+
+    G4UIExecutive *ui = nullptr;
+
+    if(argc == 1)
+    {
+        ui = new G4UIExecutive(argc, argv);
+    }
+
+    G4RunManager *runManager = new G4RunManager();
+
+    runManager->SetUserInitialization(new MyDetectorConstruction());
+    runManager->SetUserInitialization(new MyPhysicsList());
+    runManager->SetUserInitialization(new MyActionInitialization());
+    runManager->Initialize();
+
+    G4VisManager *visManager = new G4VisExecutive();
+    visManager->Initialize();
+
+    if(ui)
+    {
+        if(visualization)
+        {
+            UImanager->ApplyCommand("/control/execute vis.mac");
+            UImanager->ApplyCommand("/control/execute gps.mac");
+            
+            runManager->BeamOn(visEvents);
+            ui->SessionStart();  //activare gui
+        }
+        if(!visualization) { UImanager->ApplyCommand("/control/execute gps.mac"); }
+        delete ui;
+    }
+    else
+    {
+        G4String command = "/control/execute ";
+        G4String fileName = argv[1];
+        UImanager->ApplyCommand(command + fileName);
+    }
+
+
+    //Loop-ul cu Run-urile
+    if(!visualization)  //se executa loop-ul cu Run-uri cand visualization = false
+    {
+        for(G4int i=0; i<nOfRuns; ++i)
+        {
+            G4Random::setTheSeed(time(0) + i);
+            runManager->BeamOn(eventsPerRun);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(1500));  //time-delay intre Run-uri pt. a ii da sistemului timp sa elibereze memoria
+        }
+    }
+
+
+    delete visManager;
+    delete runManager;
+
+    return 0;
+}
